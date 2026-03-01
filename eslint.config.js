7a1d97c60b47ab9451f56bbe1f4a792e3da2d0dd
Justin <justin022306@gmail.com>
2026-03-01 04:26:37 -0500

    fix: lint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,15 @@
 import js from '@eslint/js';
+import globals from 'globals';
 
 export default [
   js.configs.recommended,
   {
     languageOptions: {
       ecmaVersion: 'latest',
-      sourceType: 'module'
+      sourceType: 'module',
+      globals: {
+        ...globals.node
+      }
     },
     rules: {
       'no-unused-vars': 'warn',
